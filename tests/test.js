3b8921208b44ea6095bdbbf540e0b0c789d77daf
franckysolo <franckysolo@gmail.com>
2016-05-01 02:37:30 +0200

    Add comments to test file
    Signed-off-by: franckysolo <franckysolo@gmail.com>

--- tests/test.js
+++ tests/test.js
@@ -3,8 +3,10 @@
  */
 var fc = require('../index.js')
 
+// Access to console with fc.cmd
 fc.cmd.time('my-timer')
 
+// Some colorized messages
 fc.crit('critical message in purple')
 fc.error('error message in red')
 fc.warn('warn message in yellow')
@@ -13,7 +15,7 @@ fc.success('success message in green')
 fc.log('log message in white')
 fc.colorize('A custom message in blue', fc.styles.inverse + fc.colors.blue)
 
-// with parameter
+// Message with parameter
 var myVar = 15;
 fc.log('the value of my var is %d', myVar)
 
